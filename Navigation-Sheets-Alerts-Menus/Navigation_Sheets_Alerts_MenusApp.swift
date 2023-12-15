b0b7f069955c42c48762e8340b29fd4d5936dde2
AnriBeridzee <aberidze02@gmail.com>
2023-12-15 18:47:46 +0400

    Initial Commit

---
+++ Navigation-Sheets-Alerts-Menus/Navigation_Sheets_Alerts_MenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_Sheets_Alerts_MenusApp.swift
+//  Navigation-Sheets-Alerts-Menus
+//
+//  Created by Macbook Air 13 on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_Sheets_Alerts_MenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
